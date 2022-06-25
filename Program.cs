a0c6937c95ed28e275324ed72786b47aefe79da0
Justin Ryan <jryan@roblox.com>
2022-06-24 21:37:07 -0700

    Generalize arguments and start to use Figma design

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using CommandLine;
+﻿using System.Diagnostics;
+using CommandLine;
 using iText.Kernel.Pdf;
 using iText.Layout;
 using iText.Kernel.Geom;
@@ -6,37 +7,103 @@ using iText.Layout.Element;
 using iText.Layout.Properties;
 using iText.Kernel.Pdf.Canvas;
 using iText.IO.Image;
+using iText.Kernel.Colors;
 using iText.Layout.Borders;
-using iText.Layout.Renderer;
+using static System.String;
 
 // See https://aka.ms/new-console-template for more information
 
 namespace Halfempty.Nametag
 {
 
-    class Generator
+    interface ITemplate
     {
+        public void Generate(Dictionary<string, string> fields, FileStream output);
+    }
 
-        private string FullName;
-        private string Title;
-        
-        private FileStream Output;
-
-        public Generator(string fullName, string title, FileStream output)
+    class FigmaTemplate : ITemplate
+    {
+        public void Generate(Dictionary<string, string> fields, FileStream output)
         {
-            FullName = fullName;
-            Title = title;
-            Output = output;
-        }
-
-        public void Generate()
-        {
-            if(Output == null || String.IsNullOrWhiteSpace(FullName) || String.IsNullOrWhiteSpace(Title))
+            var team = fields["Team"];
+            var fullName = fields["FullName"];
+            string? tagline;
+            fields.TryGetValue("TagLine", out tagline);
+            
+            if(output == null || IsNullOrWhiteSpace(fullName) || IsNullOrWhiteSpace(team))
             {
                 throw new ArgumentNullException("Output was null");
             }
 
-            var writer = new PdfWriter(Output);
+            var background = new DeviceRgb(100, 126, 221);
+            
+            var writer = new PdfWriter(output);
+            var pdfDocument = new PdfDocument(writer);
+            var ps = PageSize.LETTER;
+            var page = pdfDocument.AddNewPage(ps);
+            var canvas = new PdfCanvas(page);
+
+            Console.WriteLine("Generating file...");
+
+            float fontSize = 40;
+            var height = ps.GetHeight() / 4;
+            var forImage = ps.GetWidth() / 4;
+            var forText = ps.GetWidth()-forImage;
+            
+            Paragraph teamParagraph = new Paragraph(team)
+                .SetMargin(1)
+                .SetMultipliedLeading(1)
+                .SetMarginTop(10)
+                .SetMarginBottom(10)
+                .SetFontSize(16)
+                .SetTextAlignment(TextAlignment.CENTER)
+                //.SetFontFamily("Cabin")
+                .SetFontColor(DeviceRgb.WHITE)
+                .SetMaxWidth(ps.GetWidth());
+
+            Paragraph fullNameParagraph = new Paragraph(fullName)
+                .SetMargin(10)
+                .SetMultipliedLeading(1)
+                .SetFontSize( (int) (fontSize*.7))
+                .SetTextAlignment(TextAlignment.CENTER)
+                .SetMaxWidth(forText);
+            
+            var rect = new Rectangle(0, 0, ps.GetWidth(), height);
+
+            // Background coloring
+            canvas.SaveState();
+            canvas.SetFillColor(background);
+            canvas.Rectangle(0, 0, ps.GetWidth(), height);
+            canvas.Fill();
+            canvas.SetFillColor(DeviceRgb.WHITE);
+            var innerBorder = 15;
+            var innerBorderTop = 40;
+            canvas.Rectangle(innerBorder, innerBorderTop, ps.GetWidth()-innerBorder*2, height - innerBorderTop*2);
+            canvas.Fill();
+            canvas.RestoreState();
+            
+            new Canvas(canvas, rect)
+                .Add(teamParagraph);
+            
+            canvas.Rectangle(rect);
+            
+            pdfDocument.Close();
+
+        }
+    }
+    class Generator : ITemplate
+    {
+        public void Generate(Dictionary<string, string> fields, FileStream output)
+        {
+            var fullName = fields["FullName"];
+            var title = fields["Title"];
+                
+            if(output == null || IsNullOrWhiteSpace(fullName) || IsNullOrWhiteSpace(title))
+            {
+                throw new ArgumentNullException("Output was null");
+            }
+
+            var writer = new PdfWriter(output);
             var pdfDocument = new PdfDocument(writer);
             var ps = PageSize.LETTER;
             var page = pdfDocument.AddNewPage(ps);
@@ -56,14 +123,14 @@ namespace Halfempty.Nametag
             canvas.AddImageAt(avatar, 40, 40, true);
             //canvas.AddImageWithTransformationMatrix(avatar, -1, 0, 0, -1, 15, 15);
             
-            Paragraph nameParagraph = new Paragraph(FullName)
+            Paragraph nameParagraph = new Paragraph(fullName)
                 .SetMargin(30)
                 .SetMultipliedLeading(1)
                 .SetFontSize(fontSize)
                 .SetTextAlignment(TextAlignment.CENTER)
                 .SetMaxWidth(forText);
 
-            Paragraph titleParagraph = new Paragraph(Title)
+            Paragraph titleParagraph = new Paragraph(title)
                 .SetMargin(10)
                 .SetMultipliedLeading(1)
                 .SetFontSize( (int) (fontSize*.7))
@@ -99,15 +166,9 @@ namespace Halfempty.Nametag
             [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
             public bool Verbose {get; set;}
 
-            [Option('n', "name", Required = true, HelpText = "Full name")]
-            public string? FullName {get; set;}
-
-            [Option('t', "title", Required = true, HelpText = "Title or Team")]
-            public string? Title {get; set;}
-
-            [Option('a', "avatar", Required = false, HelpText = "Avatar name")]
-            public string? AvatarName {get; set;}
-
+            [Option(shortName:'t', longName:"templateData", Required = true, HelpText = "Template fields pairs (FullName, Team, TagLine), e.g. Team=Account Identity")]
+            public IEnumerable<string> TemplateFields { get; set; }
+            
             [Option('o', "output", Required = false, HelpText = "File to save output to")]
             public string? Output {get; set;}            
         }
@@ -117,9 +178,10 @@ namespace Halfempty.Nametag
             var result = Parser.Default.ParseArguments<GenerateOptions>(args)
                 .WithParsed<GenerateOptions>( o =>
                 {
-                    FileStream output = new FileStream(o.Output == null ? "result.pdf" : o.Output, FileMode.Create);
-                    var generator = new Generator(o.FullName!, o.Title!, output);
-                    generator.Generate();
+                    FileStream output = new FileStream(o.Output ?? "result.pdf", FileMode.Create);
+                    var fieldDictionary = o.TemplateFields.Select(t => t.Split('=')).ToDictionary(t => t[0], t => t[1]);
+                    var generator = new FigmaTemplate();
+                    generator.Generate(fieldDictionary, output);
                 });
         }
     }
